d7f7f26d6b2416d0933b8d5bd8742ae158055751
Anki908 <89961867+Anki908@users.noreply.github.com>
2021-09-10 22:12:51 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -9,41 +9,107 @@
  *
  *****************************************************************************/
 /**
- * @file <stats.h>
- * @brief my first assessment
+ * @file stats.h 
+ * @brief Definitions and macros required for the c1m1 assignment
  *
- * 
  *
  * @author ankit saini
- * @date 2021-09-03
+ * @date 10/09/2021
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+/**
+ * @brief Prints the statistics of a given array
+ *
+ * This function takes the resulting statistics done on an array
+ * which are: minimum, maximum, mean and median, and print those
+ * values on the screen
+ * 
+ * @param minimum The minimum number of the given array
+ * @param maximum The maximum number of the given array
+ * @param mean The mean of the given array
+ * @param median The median of the given array
+ *
+ * @return void
+ */
+void print_statistics (unsigned char minimum, unsigned char maximum, float mean, unsigned char median);
+
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Prints the contents of a given array
  *
- * <Add Extended Description Here>
+ * This function takes the given array and prints it to the
+ * screen.
+ * 
+ * @param array The first element of the array to be printed on the screen
  *
- * @param arr_pointer : A unsigned char pointer to an n-element data array
- * @param arr_size : An unsigned integer as the size of the array
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return An unsigned char result from the function
+ * @return void
  */
+void print_array (unsigned char *array, unsigned int counter);
 
 
-unsigned char print_statics(unsigned char *arr_pointer, unsigned int arr_size);
-unsigned char print_array(unsigned char *arr_pointer, unsigned int arr_size);
-unsigned char find_median(unsigned char *arr_pointer,unsigned int arr_size);
-unsigned char find_mean(unsigned char *arr_pointer, unsigned int arr_size);
-unsigned char find_maximum(unsigned char *arr_pointer, unsigned int arr_size);
-unsigned char find_minimum(unsigned char *arr_pointer, unsigned int arr_size);
-unsigned char sort_array(unsigned char *arr_pointer, unsigned int arr_size);
+/**
+ * @brief Finds the median of the given array
+ *
+ * This function takes the given array and finds
+ * its median.
+ * 
+ * @param array The first element of the array to be processed
+ * @param 
+ *
+ * @return median The median value of the given array.
+ */
+unsigned char find_median (unsigned char *array, unsigned int counter);
 
+/**
+ * @brief Finds the mean of the given array
+ *
+ * This function takes the given array and finds
+ * its mean.
+ * 
+ * @param array The first element of the array to be processed
+ *
+ * @return mean The mean value of the given array.
+ */
+float find_mean (unsigned char *array, unsigned int counter);
+
+/**
+ * @brief Finds the maximum of the given array
+ *
+ * This function takes the given array and finds
+ * its maximum.
+ * 
+ * @param array The first element of the array to be processed
+ *
+ * @return maximum The maximum value of the given array.
+ */
+unsigned char find_maximum (unsigned char *array, unsigned int counter);
+
+/**
+ * @brief Finds the minimum of the given array
+ *
+ * This function takes the given array and finds
+ * its minimum.
+ * 
+ * @param array The first element of the array to be processed
+ *
+ * @return minimum The minimum value of the given array.
+ */
+unsigned char find_minimum (unsigned char *array, unsigned int counter);
+
+/**
+ * @brief Sorts the given array from the largest to smallest
+ *
+ * This function takes the given array and its length, sorts
+ * the array from the largest to smallest using bubble sort and returns the
+ * sorted array.
+ * 
+ * @param array The first element of the array to be processed
+ *
+ * @return median The median value of the given array.
+ */
+void sort_array (unsigned char *array, unsigned int counter);
 
 #endif /* __STATS_H__ */
